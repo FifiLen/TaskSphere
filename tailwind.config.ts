a1478ea4790550b622f6d61f29c23e6120707e63
FifiLen <filippraca02@gmail.com>
2023-10-24 20:47:57 +0200

    front

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,11 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      'dark': '#101010',
+      'red': '#FF6060',
+      'white': '#FFF',
+      'gray': '#656262',
     },
   },
   plugins: [],
